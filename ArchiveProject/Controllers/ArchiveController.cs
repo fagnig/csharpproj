d4d174d694e4d5d7998e0f03b2078508a7717a8f
fagnig <fagnig@autistklassen.dk>
2019-05-10 15:38:36 +0200

    stage

--- ArchiveProject/Controllers/ArchiveController.cs
+++ ArchiveProject/Controllers/ArchiveController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArchiveProject.Data;
+using ArchiveProject.Logic;
+using ArchiveProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +12,36 @@ namespace ArchiveProject.Controllers
 {
     public class ArchiveController : Controller
     {
+
+        private readonly ApplicationDbContext dbContext;
+
+        public ArchiveController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Data(string id)
+        {
+
+            ModelPopulator mdl = new ModelPopulator(dbContext);
+
+
+            ArchiveViewModel tmp = mdl.GetTable(string.IsNullOrEmpty(id) ? "ArchiveMapping" : id);
+            tmp.tableHash = string.IsNullOrEmpty(id) ? "ArchiveMapping" : id;
+            tmp.tableTitle = string.IsNullOrEmpty(id) ? "ArchiveMapping" : id;
+            return View(tmp);
+        }
+
+        public void UpdateDbValue(string id, string column, string table, string value)
+        {
+            ModelUpdater mu = new ModelUpdater(dbContext);
+
+            mu.updateFields(id, column, table, value);
         }
     }
 }
---
+++ ArchiveProject/Logic/UserValidator.cs
@@ -0,0 +1,46 @@
+﻿using ArchiveProject.Data;
+using ArchiveProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArchiveProject.Logic
+{
+    public class UserValidator
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UserValidator(ApplicationDbContext context)
+        {
+            dbContext = context;
+
+        }
+
+
+        public List<int> getUserRoles(string userHash)
+        {
+
+            dbContext.sqlCon.Open();
+
+            DbCommand dc = dbContext.sqlCon.CreateCommand();
+            dc.CommandText = $"SELECT * FROM ArchiveUserRoleMapping WHERE id_user = '{userHash}'";
+      
+            DbDataReader dr = dc.ExecuteReader();
+
+            List<int> tmpList = new List<int>();
+
+            while (dr.Read())
+            {
+                tmpList.Add((int)dr.GetValue(1));
+            }
+
+            dbContext.sqlCon.Close();
+
+            return tmpList;
+        }
+    }
+}
