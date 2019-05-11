41e8baa9e7c65a2a798b976186798babd00ec786
nwi <s143780@student.dtu.dk>
2019-05-11 14:40:12 +0200

    Added Admin site

--- ArchiveProject/wwwroot/js/site.js
+++ ArchiveProject/wwwroot/js/site.js
@@ -26,9 +26,7 @@ $(document).ready(function () {
                         inputvalue = fields[i].checked;
                     } else {
                         inputvalue = fields[i].value;
-                    }
-                    alert(inputvalue);
-                   
+                    }                   
 
                     $.get('../UpdateDbValue/'
                         + fields[i].dataset.id + '?'
@@ -40,63 +38,5 @@ $(document).ready(function () {
             }
 
         });
-
-
-
-
-    //$(".registrering-submit").unbind('change').bind('change', function () {
-    //    var that = $(this);
-    //    var indexIdTemp = that[0].id;
-    //    var indexId = indexIdTemp.substr(0, indexIdTemp.lastIndexOf('_') + 1);
-    //    var index = $("#" + indexId + "Id")[0].value;
-
-    //    var expectedVal = $("#" + indexId + "Forventet")[0].value;
-    //    var currentVal = $("#" + indexId + "Realiseret")[0].value;
-    //    var deviationVal = $("#" + indexId + "Afvigelse")[0].value;
-    //    $.get('Registrations/SetHourlyRegistrations/?expected=' +
-    //        expectedVal +
-    //        '&current=' +
-    //        currentVal +
-    //        '&deviation=' +
-    //        deviationVal +
-    //        '&sId=' +
-    //        index).done(function () {
-    //            SumRegistreringer();
-    //        });
-    //});
-
-
-    //$(".team-escalation").unbind('click').bind('click',
-    //    function (e) {
-    //        e.preventDefault();
-    //        var buttons = this.parentNode.children;
-    //        buttons[0].classList = "btn team-escalation team-escalation-not-chosen";
-    //        buttons[1].classList = "btn team-escalation team-escalation-not-chosen";
-    //        $.get('SetEscalation/' +
-    //            this.dataset.regId +
-    //            '?type=' +
-    //            this.dataset.type +
-    //            '&choice=' +
-    //            this.dataset.choice);
-    //        this.classList = "btn team-escalation team-escalation-chosen";
-    //    });
-
-    //function SumRegistreringer() {
-    //    var listRealiseret = $('[id$=__Realiseret]');
-    //    var totalRealiseret = 0;
-
-    //    listRealiseret.each(function () {
-    //        totalRealiseret += Number.parseFloat($(this)[0].value.replace(',', '.'));
-    //    });
-    //    $("#totalrealiseret")[0].value = String(totalRealiseret.toFixed(2)).replace('.', ',');
-
-    //    var listForventet = $('[id$=__Forventet]');
-    //    var totalForventet = 0;
-    //    listForventet.each(function () {
-    //        totalForventet += Number.parseFloat($(this)[0].value.replace(',', '.'));
-    //    });
-    //    $("#totalforventet")[0].value = String(totalForventet.toFixed(2)).replace('.', ',');
-    //}
-
 });
-
+ 
